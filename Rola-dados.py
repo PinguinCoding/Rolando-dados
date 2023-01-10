6f0665d0c61a8dfc60f02362f104267fdac12463
Guilherme <112185084+PinguinCoding@users.noreply.github.com>
2023-01-10 19:46:53 -0300

    Update Rola-dados.py

--- Rola-dados.py
+++ Rola-dados.py
@@ -6,11 +6,13 @@ resultado = {"Jogador1": 0, "Jogador2": 0, "Jogador3": 0, "Jogador4": 0}
 
 for key in resultado.keys():
     resultado[key] = randint(1, 6)
+# Rola os dados / Gera um número aleatório de 1 a 6 e preenche nas posições do dicionário
 
 for i, key in enumerate(resultado.keys()):
     print(f'O dado que o {i+1}° Jogador tirou foi: {resultado[key]}')
     ranking.append(resultado[key])
     sleep(0.5)
+# Mostra os dados rolados na tela / Adicionado as rolagens na lista 'ranking'
 
 print('=+'*30)
 
@@ -23,9 +25,13 @@ for c in range(0, len(ranking)):
         if ranking.count(ranking[c]) == 1 and ranking[c] == resultado[key]:
             print(f'O {c + 1}° colocado é {key} com {resultado[key]}')
             sleep(0.5)
+            # Verifica se não houve dados repetidos e se o valor do dado na ordem de ranking é igual ao valor tirado pelo jogador 
+            # Alinha o resultado do dado salvo em ranking com o jogador que tirou o dado
 
         if ranking.count(ranking[c]) > 1 and ranking[c] == resultado[key]:
             print(f'O {c + 1}° colocado é {key} com {resultado[key]}')
             sleep(0.5)
             del resultado[key]
             break
+            # Verifica se houve dados repetidos e se o valor dado na ordem de ranking é igual ao valor tirado pelo jogador
+            # Alinha o resultado do dado salvo em ranking com a primeira ocorrência do jogador que tirou o dado
